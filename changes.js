6fce4a480696d0227eabb18f1de8c1f3a17b7924
rizwanpizzee <rizwanpizzee@gmail.com>
2024-12-09 15:20:44 +0500

    version 15

--- changes.js
+++ changes.js
@@ -1,4 +1,5 @@
 console.log("new cahnges made");
 console.log("changes mad after push 1");
 console.log("Push at 12/9/2024 02:54 PM");
-console.log("Push at 12/9/2024 03:14 PM");
+console.log("Push at 12/9/2024 03:14 PM");   
+console.log("Push at 12/9/2024 03:21 PM");  
